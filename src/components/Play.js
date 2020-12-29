4418857676f07c9f0476f504d149e7e92a381e1b
Fandrew44 <ahgreeting@gmail.com>
2020-12-28 17:46:02 -0700

    Fix random target generation algorithm

--- src/components/Play.js
+++ src/components/Play.js
@@ -16,56 +16,97 @@ const Play = props => {
     const pushToRoute = route => {
         props.history.push(route);
     }
-    
 
-    //This takes WAY TOOOOOOO LONG 
-    //Need a BETTER ALGORITHM to generate NON-OVERLAPPING CIRCLES
+
+    //Wait no this algorithm is pretty fast actually it just DOES NOT WORK ( b r u h )
+    // - allTargets working CORRECTLY
+    // - setTargets DOES NOT seem to be MODIFYING the 'targets' state <= PROBLEM
+    // - ...targets DOES WORK ?
+    // - Is it the WHILE LOOP that's breaking it?
+    //IMPORTANT NOTE: Manipulating States in React is ASYNCHRONOUS
     const startGame = () => {
+        // setInGame(true);
+        // //Works in this scenario
+        // setTargets([{
+        //     x: 1,
+        //     y: 2,
+        //     r: 3,
+        //     id: 4
+        // }]);
+        // const target = {
+        //     x: 100,
+        //     y: 101,
+        //     r: 103,
+        //     id: 144,
+        // }
+        // while (targets.length < 4) {
+        //     console.log("Adding new target");
+        //     setTargets([
+        //         ...targets,
+        //         target,
+        //     ])
+        // }
+        // setTargets([
+        //     ...targets,
+        //     target
+        // ])
+        //NOTE: Since modifying states in React is ASYNC, I can just first create the Array with all the appropriate Targets and then
+        //simply set the 'targets' state to that Array
+        // - Rather than updating the 'targets' state with each individual target generated
         setInGame(true);
         let allTargets = [];
-        while (targets.length < 44) {
-            console.log("Beep boop generating targets.");
+        let displayTargets = [];
+        while (displayTargets.length < 44) {
+            console.log(`# of targets is: ${targets.length}`);
+            // console.log("Beep boop generating targets.");
             let overlapping = false;
-            let xMin = 25;
-            let xMax = 795
-            let yMin = 25;
-            let yMax = 700;
-            let target = {
+            const xMin = 25;
+            const xMax = 850
+            const yMin = 25;
+            const yMax = 700;
+            const target = {
                 x: Math.floor(Math.random() * (xMax - xMin + 1)) + xMin,
                 y: Math.floor(Math.random() * (yMax - yMin + 1)) + yMin,
                 r: 1,
+                id: (Math.random() * 1000),
             };
-            allTargets.push(target);
+
             for (let i = 0; i < allTargets.length; i++) {
                 let other = allTargets[i];
                 let xDiff = target.x - other.x;
                 let yDiff = target.y - other.y;
                 let dist = Math.sqrt(xDiff * xDiff + yDiff * yDiff);
-                if (dist < (target.r + other.r)) {
+                if (dist < 2) {
                     overlapping = true;
                     break;
                 }
             }
+            allTargets.push(target);
 
-            if (!overlapping) {
-                targets.push(target);
+            //IMPORTANT NOTE: Remember that 'targets' is a STATE
+            // - It MUST be modified using the SPECIFIED FUNCTION returned from useState()
+            if (!overlapping) { 
+                // console.log("Adding target to State");
+                // console.log(target.id);
+                displayTargets.push(target);
             }
         }
-    
+
+        setTargets(displayTargets);
     }
 
     const endGame = () => {
         setInGame(false);
     }
- 
-    
-    return(
+
+
+    return (
         <div>
             <header>
                 <button className="start-button" onClick={ startGame }>Start</button>
                 <BrowserRouter>
                     <Switch>
-                        <button className="menu-back start-button" onClick={() => {pushToRoute('/')}}>
+                        <button className="menu-back start-button" onClick={() => { pushToRoute('/') }}>
                             Menu
                         </button>
                     </Switch>
@@ -73,7 +114,7 @@ const Play = props => {
             </header>
             <div className="game-screen">
                 {targets.map(target => (
-                    <Target target={ target } />
+                    <Target target={target} color="blue"/>
                 ))}
             </div>
         </div>
--- src/components/Target.js
+++ src/components/Target.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import '../Target.css';
+import style from 'styled-components';
 
 
-const Target = ({ target }) => {
+// const Target = ({ target }) => {
+//     const top = target.y;
+//     const left = target.x;
 
-    return(
-        <div className="target"></div>
-    );
-}
+//     return(
+//         <div className="target" style={
+//             {top: {top},
+//              left: {left}
+//             }}></div>
+//     );
+// }
+
+//styled-components (in this case) allows for PROPS to directly dictate the styling (i.e. position) of the React Components
+const Target = style.div`
+    position: absolute;
+    bottom: ${props => props.target.y};
+    right: ${props => props.target.x};
+    width: 2rem;
+    height: 2rem;
+    background: black;
+    /* -moz-border-radius: 50px;
+    -webkit-border-radius: 50px;
+    border-radius: 50px; */
+  
+    color: black;
+    border-radius: 50%;
+    background-color: #c00;
+    background-clip: content-box;
+    padding: 0.25rem;
+    border: solid 0.5rem #c00;
+    /* background: repeating-radial-gradient(red, red 10%, white 10%, white 20%); */
+    transition: border-color 200ms ease-in, background-color 200ms ease-in;
+    animation-name: zoomInOut;
+    animation-duration: 5s;
+    animation-fill-mode: both;
+    
+    &:hover {
+        border: solid 0.5rem rgb(243, 136, 136);
+    }
+
+
+`;
+
 
 export default Target;
